389801750c308994fca0d5555d0be8a211dcc220
Mohamed Altorky <mohamed.altorky@liangdao.de>
2024-11-23 13:54:46 +0100

    created path class

---
+++ py-project/path.py
@@ -0,0 +1,10 @@
+class Path():
+    def __init__(self, car, customer) -> None:
+        self.car = car
+        self.customer = customer
+        self.distance = 0
+        self.co2_emission = 0
+        self.energy_consumption = 0
+        self.travel_time = 0
+        self.trip_cost = 0
+    
